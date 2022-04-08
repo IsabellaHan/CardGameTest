d3d2889665aba161540abbebe278e873efa5ffe3
IsabellaHan <isabella.sa.han@gmail.com>
2022-04-08 11:13:25 -0400

    Creating the deck and shuffling

---
+++ My project/Assets/Scripts/Solitaire/CardType.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct CardType 
+{
+    public enum Suites
+    {
+        Hearts = 0,
+        Diamonds,
+        Clubs,
+        Spades
+    }
+
+    public int Value
+    {
+        get;
+        set;
+    }
+
+    public Suites Suite
+    {
+        get;
+        set;
+    }
+
+    public string NamedValue
+    {
+        get
+        {
+            string name = string.Empty;
+            switch (Value)
+            {
+                case (14):
+                    name = "Ace";
+                    break;
+                case (13):
+                    name = "King";
+                    break;
+                case (12):
+                    name = "Queen";
+                    break;
+                case (11):
+                    name = "Jack";
+                    break;
+                default:
+                    name = Value.ToString();
+                    break;
+            }
+
+            return name;
+        }
+    }
+
+    public string Name
+    {
+        get
+        {
+            return NamedValue + " of  " + Suite.ToString();
+        }
+    }
+
+    public CardType(int Value, Suites Suite)
+    {
+        this.Value = Value;
+        this.Suite = Suite;
+    }
+}
+
---
+++ My project/Assets/Scripts/Solitaire/solitaire.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class solitaire : MonoBehaviour
+{
+ 
+    public Sprite[] cardSprites;
+    public GameObject cardObj;
+
+    public List<CardType> deck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        deck = GenerateDeck();
+        ShuffleDeck(deck);
+       
+    }
+
+
+
+
+    List<CardType> newDeck = new List<CardType>();
+    private List<CardType> GenerateDeck()
+    {
+
+
+        for (int i = 0; i < 52; i++)
+        {
+            // Math.Floor() function returns the largest integer less than or equal to a given number.
+            CardType.Suites suite = (CardType.Suites)(Math.Floor((decimal)i / 13));
+            //Add 2 to value as a cards start a 2
+            int val = i % 13 + 2;
+            newDeck.Add(new CardType(val, suite));
+        }
+        return newDeck;
+
+    }
+
+    void ShuffleDeck<CardType>(List<CardType> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            CardType temp = list[i];
+            int randomIndex =UnityEngine.Random.Range(i, list.Count);
+            // in the i'th position in list, place the random thing in range of list.count
+            list[i] = list[randomIndex]; // the i'th thing in the list is placed in a random place in list
+            list[randomIndex] = temp;
+        }
+    }
+
+    public void ShuffleButton() {
+        ShuffleDeck(deck);
+        foreach (CardType c in deck)
+        {
+            Debug.Log(c.Name);
+        }
+    }
+
+    public void DealCards() {
+        foreach (CardType c in deck) {
+            GameObject newCard = Instantiate(cardObj, transform.position, Quaternion.identity);
+            newCard.name = c.Name;
+        }
+    }
+}
